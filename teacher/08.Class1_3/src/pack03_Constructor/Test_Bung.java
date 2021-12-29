40af0ab9afa2c3ed540ca5a3eed2be5d1aa51b32
seongukchoi <cabin2351@gmail.com>
2021-12-29 20:05:00 +0900

    12/29 20:04 choi commit

---
+++ teacher/08.Class1_3/src/pack03_Constructor/Test_Bung.java
@@ -0,0 +1,16 @@
+package pack03_Constructor;
+
+public class Test_Bung {
+	String in_Item;
+	int price;
+	String name;
+	String ymd;
+	public Test_Bung(String in_Item, int price, String name, String ymd) {
+		super();
+		this.in_Item = in_Item;
+		this.price = price;
+		this.name = name;
+		this.ymd = ymd;
+	}
+	
+}
